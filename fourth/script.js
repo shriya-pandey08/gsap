a156444876923d70d6e446b035cf98a7fb5a94f6
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-08-26 00:47:15 +0530

    staggered

---
+++ fourth/script.js
@@ -0,0 +1,18 @@
+gsap.to("#box", {
+  x:1000,
+  duration: 2,
+  delay:1,
+  rotate:180,
+  backgroundColor:"pink",
+  borderRadius:"50%",
+  scale:0.2,
+  repeat:-1
+})
+gsap.from("#box2",{
+  x:1000,
+  duration:2,
+  delay:1,
+  rotate:360,
+  backgroundColor:"orange",
+  borderRadius:"50%"
+})
