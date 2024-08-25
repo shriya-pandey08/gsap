14ab1b518a39ddff90036f3f011cd55836f83e49
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-08-26 00:41:20 +0530

    initial

--- third/script.js
+++ third/script.js
@@ -4,10 +4,15 @@ gsap.to("#box", {
   delay:1,
   rotate:180,
   backgroundColor:"pink",
+  borderRadius:"50%",
+  scale:0.2,
+  repeat:2
 })
 gsap.from("#box2",{
   x:1000,
   duration:2,
   delay:1,
   rotate:360,
+  backgroundColor:"orange",
+  borderRadius:"50%"
 })
