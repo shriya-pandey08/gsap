44e8e9b97eeda6035f3baa632bb88813f6ec2012
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-08-30 15:18:52 +0530

    use-of-scrub-property

--- nine/script.js
+++ nine/script.js
@@ -1,22 +1,31 @@
-gsap.from("#page2 h1",{
-   opacity:0,
-   duration:2,
-   x:500,
-   scrollTrigger:{
-    trigger:"#page2 h1",
-    scroller: body,
-    markers:true,
-    start: "top 50%"
-   }
-})
-gsap.from("#page2 h1",{
-  opacity:0,
+gsap.from("#page1 #box",{
+  scale:0,
+  delay:1,
   duration:2,
-  x:-500,
+  rotate:360
+})
+
+gsap.from("#page2 #box",{
+  scale:0,
+  duration:2,
+  rotate:720,
+  // scrollTrigger:"#page2 #box"  
+  //this helps to create animation when the screen is scrolled 
+  //this can be also written in the detail form by creating object- given below 
+
   scrollTrigger:{
-   trigger:"#page2 h1",
-   scroller: body,
-   markers:true,
-   start: "top 50%"
+    trigger:"#page2 #box",
+    scroller:"body",
+    markers:true,
+    start:"top 60%", // delay command is removed while using this 
+    end: "top 30%",
+    scrub: 2 
   }
+})
+gsap.from("#page3 #box",{
+  scale:0,
+  delay:1,
+  duration:2,
+  rotate:60,
+  scrollTrigger:"#page3 #box"
 })
